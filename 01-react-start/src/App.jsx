462ea0fd746d80357c50f6667bd5f9114043cbce
Bekhzod Bakhshilloev <beki2627@gmail.com>
2024-02-28 11:12:15 +0500

    first commit

--- 01-react-start/src/App.jsx
+++ 01-react-start/src/App.jsx
@@ -1,35 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
+
+
+// Компоненты это JS функции которые могут возвращать jsx элементы и внутри себя могут содержать какое-то поведение.
+// Этот функция является компонентом App. Заметьте что все компоненты называются большой буквой.
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return <div className="App"></div>;
 }
 
-export default App
+export default App;
