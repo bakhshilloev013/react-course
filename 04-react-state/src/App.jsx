f5c0705fd2bb1a8801830f830bce697edafcc2e8
Bekhzod Bakhshilloev <beki2627@gmail.com>
2024-03-08 17:00:30 +0500

    state v3

--- 04-react-state/src/App.jsx
+++ 04-react-state/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
 
+// props - это объект со свойтсвами переденными в этот компонент
 function WhoAmI(props) {
+    // исходя из того что props это объект его можно деструктурировать либо в поле для аргументов либо обыденным нам способом:
+    const { name, surname, link } = props;
     return (
         <div>
             <h1>
-                My name is {props.name}, surname - {props.surname}
+                My name is {name}, surname - {surname}
             </h1>
-            <a href={props.link}>My profile</a>
+            <a href={link}>My profile</a>
         </div>
     );
 }
@@ -14,6 +17,7 @@ function WhoAmI(props) {
 function App() {
     return (
         <div>
+            {/* значаения для свойств компонента передаются в виде атрибутов, эти атрибуты будут свойствами в объекте свойств props */}
             <WhoAmI name="John" surname="Smith" link="instagram.com" />
         </div>
     );
