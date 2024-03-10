4cdd4d5e2a972152f9a205a920603eeb388abbb2
Bekhzod Bakhshilloev <beki2627@gmail.com>
2024-03-10 17:13:17 +0500

    events reload

--- 07-react-events/src/App.jsx
+++ 07-react-events/src/App.jsx
@@ -7,6 +7,7 @@ class WhoAmI extends Component {
         this.state = {
             years: 24,
             text: '++',
+            position: '',
         };
     }
 
@@ -16,17 +17,31 @@ class WhoAmI extends Component {
         }));
     };
 
+    commitInputValue = (e) => {
+        this.setState({
+            position: e.target.value,
+        });
+    };
 
     render() {
         const { name, surname, link } = this.props;
+        const { position, years } = this.state;
         return (
             <div>
                 <button onClick={this.nextYear}>{this.state.text}</button>
                 <h1>
-                    My name is {name}, surname - {surname}, age - {this.state.years}
+                    My name is {name}, surname - {surname}, age - {years},
+                    position - {position || 'default'}
                 </h1>
                 <a href={link}>My profile</a>
-                
+                <form>
+                    <span>Введите должность</span>
+                    <input
+                        type="text"
+                        value={position}
+                        onChange={this.commitInputValue}
+                    />
+                </form>
             </div>
         );
     }
